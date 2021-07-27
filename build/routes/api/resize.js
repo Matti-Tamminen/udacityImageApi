2d864fd68886fdfabf6455d16e90265348b7455c
“[Matti <[matti.tamminen@codecontrol.fi]>
2021-07-27 11:54:14 +0300

    sharp

--- build/routes/api/resize.js
+++ build/routes/api/resize.js
@@ -1,7 +1,28 @@
 "use strict";
 var express = require('express');
+var sharp = require('sharp');
+var path = require('path');
 var resize = express.Router();
-resize.get('/', function (req, res) {
-    res.status(200).send('resize');
+var thumbpath = path.join(__dirname, '..', '..', 'build', 'thumb');
+resize.get('/', function (req, res, next) {
+    var _a = req.query, name = _a.name, width = _a.width, height = _a.height;
+    var filepath = path.join(__dirname, '..', '..', 'build', 'full', name + ".jpg");
+    console.log("localhost:3000/api/resize" + req.url);
+    next();
+    sharp(filepath)
+        .resize(parseInt(width), parseInt(height))
+        .toFile(path.join(thumbpath, name + "2.jpg"))
+        .catch(function (err) {
+        console.log(err);
+    });
+    res.send("<img src=\"" + path.join(thumbpath, name + "2.jpg") + "\" alt=\"kuva\" />");
 });
 module.exports = resize;
+// .jpeg({ mozjpeg: true })
+//         .toBuffer()
+//         .then((data: any) => {
+//             res.status(200).send(`<img src="${req.url}" alt="kuva">${data}</img>`)
+//         })
+// , (err: any, info: any) => {
+//     console.log(info)
+// }
--- routes/api/resize.ts
+++ routes/api/resize.ts
@@ -1,9 +1,39 @@
+import { start } from "node:repl"
+
 const express = require('express')
+const sharp = require('sharp')
+const path = require('path')
 
 const resize = express.Router()
 
-resize.get('/', (req: any, res: any) => {
-    res.status(200).send('resize')
+// path to full sized images
+const filepath = path.join(__dirname, '..', '..', 'build', 'full')
+// path to thumb sized images
+const thumbpath = path.join(__dirname, '..', '..', 'build', 'thumb')
+
+// const image = (name: string, url: string) => {
+//     var img = new Image()
+//     img.src = 'https://www.w3schools.com/html/tryit.asp?filename=tryhtml_images_trulli'
+
+
+//     return img
+// }
+
+resize.get('/', (req: any, res: any, next: any) => {
+    const { name, width, height } = req.query
+    next()
+
+    sharp(path.join(filepath, `${name}.jpg`))
+        .resize(parseInt(width), parseInt(height))
+        .toFile(path.join(thumbpath, `${name}2.jpg`))
+        .catch((err: any) => {
+            console.log(`Error from image loading: ${err}`)
+            res.status(500).send('Something went wrong, check your path.')
+        })
+    res.sendFile(path.join(thumbpath, `${name}2.jpg`))
 })
 
 export = resize
+
+// `http://localhost:3000${url}`
+// http://localhost:3000${req.originalUrl}

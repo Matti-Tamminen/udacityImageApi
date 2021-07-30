19bd148391306000097299629d9bcd2113da504d
“[Matti <[matti.tamminen@codecontrol.fi]>
2021-07-30 12:45:46 +0300

    refactor

--- build/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var express = require('express');
-var routes = require('../src/routes/main');
-var app = express();
-var PORT = 3000;
-app.use('/api', routes);
-app.listen(PORT, function () {
-    console.log("Listening on port: " + PORT);
-});
